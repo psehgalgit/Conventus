90684a6c6e6741fde81e3a051913883f0e86e516
Pankaj Sehgal <pankaj.sehgal@redrockinfosolutions.com>
2016-09-27 11:56:55 +0530

    First Commit of the conventus project

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/apply', function () {
+    return view('loanapplication');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+Route::get('/users', 'UserController@index');
+
+
+Route::get('/users/create', 'UserController@create');
+
+Route::post('/users/store', 'UserController@store');
+
+
+
