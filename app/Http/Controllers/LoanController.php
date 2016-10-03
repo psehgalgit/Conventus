5734de37e9b9e14b3898b2750c0357b288274ba6
Pankaj Sehgal <pankaj.sehgal@redrockinfosolutions.com>
2016-10-03 16:54:18 +0530

    for the loan controller

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -9,4 +9,32 @@ use App\Http\Requests;
 class LoanController extends Controller
 {
     //
+	
+	public function credit()
+    {
+        //
+		return view('loan.credit');
+    }
+	
+	
+	
+	public function property()
+    {
+        //
+		return view('loan.propertyinfo');
+    }
+	
+	
+	public function borrower()
+    {
+        //
+		return view('loan.borrower');
+    }
+	
+	public function entity()
+    {
+        //
+		return view('loan.entity');
+    }
+	
 }
